3daa7ed01baacdad8cbdfd7593b590a6cb07133e
Will Collings <william.collings@rockets.utoledo.edu>
2017-11-29 13:58:15 -0500

    loading works

--- HashTable.java
+++ HashTable.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 public class HashTable{
 	private int num_ele;
 	String[] table;
@@ -18,14 +20,17 @@ public class HashTable{
 
 	private void constructor() //does the actual constructing
 	{
+		table = new String[size];
 		num_ele=0;
 		powers= new int[4];
 		powers[0]=29791;
 		powers[1]=961;
 		powers[2]=31;
 		powers[3]=1;
-		table = new String[size];
-
+		for (int i=0; i < size; ++i)
+		{
+			table[i]="";
+		}
 	}
 	public void insert(String toHash)
 	{
@@ -36,21 +41,22 @@ public class HashTable{
 		}
 	}
 
-	private double hashValue(String toHash)
+	private int hashValue(String toHash)
 	{
 		double result=0;
-		for (int j=0; j < toHash.size(); j+=4)
+		for (int j=0; j < toHash.length(); j+=4)
 		for (int i=0; i < 4; ++i)
 		{
-			if (i+j >= toHash.size())
-				return result%size;
-			result+=(table[index].name[i+j]-10)*powers[i];
+			if (i+j >= toHash.length())
+				return Math.abs((int)(result%size));
+			else result+=(Character.getNumericValue(toHash.charAt(i+j))-10)*powers[i];
 		}
+		return Math.abs((int)(result%size));
 	}
 
 	public boolean contains(String toFind)
 	{
-	 return table[hashValue(toFind)].isEmpty();
+		return table[hashValue(toFind)].isEmpty();
 	}
 
 	public int Count()
--- p7main.java
+++ p7main.java
@@ -5,16 +5,22 @@ public class p7main{
 	static HashTable ht;
 	static File dict;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		ht = new HashTable();
+		Scanner stdin = new Scanner(System.in);
+		System.out.println("What's the dictionary file?");
+		loadNewDictionaryFile(stdin.next());
+		System.out.println("Load finished");
+		stdin.close();
 	}
-	public static void loadNewDictionaryFile(String fname)
+	public static void loadNewDictionaryFile(String fname) throws Exception
 	{
 		ht= new HashTable();
 		dict= new File(fname);
 		if (dict.canRead())
 			ReadDictionaryFile();
 	}
-	public static void ReadDictionaryFile()
+	public static void ReadDictionaryFile() throws Exception
 	{
 		Scanner in = new Scanner(dict);
 		while (in.hasNext())

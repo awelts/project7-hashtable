53825d7002741fcd52144c10d70b292676ef34d0
Will Collings <william.collings@rockets.utoledo.edu>
2017-12-01 13:14:08 -0500

    everything but hash works

--- HashTable.java
+++ HashTable.java
@@ -1,26 +1,35 @@
 import java.lang.Math;
+import java.util.Vector;
+import java.io.File;
+import java.util.Scanner;
 
 public class HashTable{
 	private int num_ele;
 	String[] table;
 	private int[] powers;
-	private int size;
-
+	private long size;
 	public HashTable()
 	{
 		this.size=1000;
 		constructor();
 	}
 
-	public HashTable(int n)
+	public HashTable(int n) throws Exception
 	{
-		this.size=n;
+		File tempFile = new File("primes.txt");
+		Scanner in = new Scanner(tempFile);
+		int temp_n= in.nextInt();
+		while (3*n > temp_n && in.hasNext())
+			temp_n = in.nextInt();
+		size=temp_n;
+		in.close();
+		System.out.println("chose prime number " +size);
 		constructor();
 	}
 
 	private void constructor() //does the actual constructing
 	{
-		table = new String[size];
+		table = new String[(int)size];
 		num_ele=0;
 		powers= new int[4];
 		powers[0]=29791;
@@ -34,33 +43,39 @@ public class HashTable{
 	}
 	public void insert(String toHash)
 	{
-		if (!contains(toHash))
+		if (!contains(toHash, false))
 		{
 			num_ele++;
-			table[hashValue(toHash)] = toHash;
+			table[hashValue(toHash, false)] = toHash;
 		}
 	}
 
-	private int hashValue(String toHash)
+	private int hashValue(String toHash, boolean print)
 	{
 		double result=0;
-		for (int j=0; j < toHash.length(); j+=4)
-		for (int i=0; i < 4; ++i)
+		toHash=toHash.toLowerCase();
+		for (int j=0; j <= toHash.length(); j+=4)
 		{
-			if (i+j >= toHash.length())
-				return Math.abs((int)(result%size));
-			else result+=(Character.getNumericValue(toHash.charAt(i+j))-10)*powers[i];
+			for (int i=0; i < 4; ++i)
+			{
+				if (i+j >= toHash.length())
+					return (int)Math.abs((result%size));
+				result+=(Character.getNumericValue(toHash.charAt(i+j))-10)*powers[i];
+			}
 		}
+		if (print) System.out.println(result);
 		return Math.abs((int)(result%size));
 	}
 
-	public boolean contains(String toFind)
+	public boolean contains(String toFind, boolean print)
 	{
-		return table[hashValue(toFind)].isEmpty();
+		if (print) System.out.printf("%s (%d)%n",table[hashValue(toFind, print)],hashValue(toFind, print));
+		return !(table[hashValue(toFind, print)].equals(""));
 	}
 
 	public int Count()
 	{
 		return num_ele;
 	}
+
 }
